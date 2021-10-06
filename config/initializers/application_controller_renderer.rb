0ab264d903fc1f9a26923aaa650b3dc79854de51
EC2 Default User <ec2-user@ip-172-31-1-90.ec2.internal>
2021-10-06 06:37:11 +0000

    [Finish]delete-option

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '4121e8c7adf4351a2e2012c0400602bacdf1c4cefc12e0b8d6cc81549ca828e7322af967aa935a15fabe04d7ef963e76f0383abb7f0cd6e935f3c425d1367b51'
